42f544cd6352db9e97a9a78cd14f5ecb7046a62f
yuryabtseva <yuliariabtceva@gmail.com>
2023-02-13 15:45:31 -0800

    functions homework

--- tests/00_functions/functions.js
+++ tests/00_functions/functions.js
@@ -1,19 +1,49 @@
 /* eslint-disable arrow-body-style */
 /* eslint-disable consistent-return */
-function max() {
-
+function max(num1, num2, num3) {
+  var highest = 0;
+  if (num1 >= num2 && num1 >= num3) {
+    highest = num1;
+  } else if (num2 >= num1 && num2 >= num3) {
+    highest = num2;
+  } else {
+    highest = num3;
+  }
+  return highest;
 }
 
 function addUnknown() {
-
+  var sum = 0;
+  for (var i=0; i<arguments.length; i++) {
+   sum = sum + (arguments[i]);
+  }
+  return sum;
 }
 
-function evenOrOdd() {
-
+function evenOrOdd(number) {
+  var result;
+  if (number % 2 == 0) {
+    result = "even";
+  } else {
+    result = "odd";
+  }
+return result;
 }
 
-function operator() {
-
+function operator(num1, num2, num3) {
+  var result;
+  if (num1+num2==num3) {
+    result = "plus";
+  } else if (num1-num2==num3) {
+    result = "minus";
+  } else if (num1*num2==num3) {
+    result = "multiply";
+  } else if (num1/num2==num3) {
+    result = "divide";
+  } else {
+    result = null;
+  }
+  return result;
 }
 
 module.exports = {
