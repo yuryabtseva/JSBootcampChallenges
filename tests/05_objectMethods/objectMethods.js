4f67d990d3f2dc748426f83e26628be23fc066f1
yuryabtseva <yuliariabtceva@gmail.com>
2023-02-27 09:56:48 -0800

    objectMethods hw

--- tests/05_objectMethods/objectMethods.js
+++ tests/05_objectMethods/objectMethods.js
@@ -3,10 +3,26 @@
 /* eslint-disable no-undef */
 
 const greet = {
+    firstName: "Joe",
+    sayHello() {
+        return `Hello, I'm ${this.firstName}`
+    }
 };
 
 const calc = {
-
+    total: 0,
+    add(num) {
+        this.total=this.total + num;
+        return this.total;
+    },
+    subtract(num) {
+        this.total=this.total - num;
+        return this.total;
+    },
+    clear() {
+        this.total=0;
+        return 0;
+    }
 };
 
 module.exports = { greet, calc };
