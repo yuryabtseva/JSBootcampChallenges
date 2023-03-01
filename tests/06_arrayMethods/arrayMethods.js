00a7effc292baf4cf1ebdc915e9280a65c60e8ee
yuryabtseva <yuliariabtceva@gmail.com>
2023-02-28 18:59:07 -0800

    arrayMethods hw

--- tests/06_arrayMethods/arrayMethods.js
+++ tests/06_arrayMethods/arrayMethods.js
@@ -1,33 +1,65 @@
-function doubler() {
-
+function doubler(arr) {
+  return arr.map(num => num * 2);
 }
 
-function mapCap() {
-
+function mapCap(arr) {
+  return arr.map(item => item.toUpperCase());
 }
 
-function canRideTheRide() {
-
+function canRideTheRide(arr) {
+  const newArr = [];
+  arr.map(item => {
+    if (item.age > 16) {
+      newArr.push(`${item.name} can ride the ride`)
+    } else {
+      newArr.push(`Sorry, ${item.name} cannot ride the ride`)
+    }
+  });
+  return newArr;
 }
 
-function indAndVal() {
-
+function indAndVal(arr) {
+  const newArr = [];
+  let num = 0;
+  arr.map(item => {
+    newArr.push({index: num++, value: item})
+  });
+  return newArr;
 }
 
-function filterEvens() {
-
+function filterEvens(arr) {
+  const newArr = [];
+  arr.filter((num) => {
+    if (num % 2 === 0) {
+      newArr.push(num);
+    }
+  });
+  return newArr;
 }
 
-function gmailSearch() {
-
+function gmailSearch(arr) {
+  const newArr = [];
+  arr.filter((email) => {
+    if (email.includes("@gmail.com")) {
+      newArr.push(email);
+    }
+  });
+  return newArr;
 }
 
-function getCart() {
-
+function getCart(arr) {
+  const sum = arr.reduce((accumulator, iterator) => {
+    if (iterator.price < 100) {
+      return accumulator + iterator.price;
+    }
+    return accumulator;
+    }, 0);
+    return sum;
 }
 
-function findGreater() {
-
+function findGreater(arr) {
+  return arr.find(({ price }) => 
+    price > 100);
 }
 
 module.exports = {
